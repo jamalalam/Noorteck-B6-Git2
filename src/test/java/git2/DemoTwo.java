65f6b844c7fd3539c2934decc5f95873d219658f
jamal <jamal@DESKTOP-FUCFPD8.ht.home>
2022-05-05 20:30:03 -0400

    New methods added

--- src/test/java/git2/DemoTwo.java
+++ src/test/java/git2/DemoTwo.java
@@ -11,4 +11,10 @@ public class DemoTwo {
 	public void add(int a, int b) {
 		System.out.println(a+b);
 	}
+	public void subtract(int a, int b) {
+		System.out.println(a-b);
+	}
+	public void multiply(int a, int b) {
+		System.out.println(a*b);
+	}
 }
